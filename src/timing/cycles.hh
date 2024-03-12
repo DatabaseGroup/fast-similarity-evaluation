a86eb2677f689b1df52c08ff80e2aae4dc8303d9
Son_Typ <Son_Typ@riapei.at>
2024-03-12 11:32:03 +0100

    Update cycle counting for ARMv8

--- src/timing/cycles.hh
+++ src/timing/cycles.hh
@@ -6,6 +6,11 @@ namespace timing {
 typedef unsigned long long ticks;
 
 static __inline__ ticks cpu_cycles_start () {
+#if defined(__aarch64__) || defined(_M_ARM64)
+  uint64_t value;
+  asm("isb; mrs %0, PMCCNTR_EL0" : "=r"(value));
+  return value;
+#else
   unsigned cycles_low, cycles_high;
   asm volatile ("CPUID\n\t"
                "RDTSC\n\t"
@@ -13,9 +18,16 @@ static __inline__ ticks cpu_cycles_start () {
                "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
                                                           "%rax", "%rbx", "%rcx", "%rdx");
   return ((ticks)cycles_high << 32) | cycles_low;
+#endif
 }
 
+
 static __inline__ ticks cpu_cycles_stop () {
+#if defined(__aarch64__) || defined(_M_ARM64)
+  uint64_t value;
+  asm("mrs %0, PMCCNTR_EL0" : "=r"(value); isb);
+  return value;
+#else
   unsigned cycles_low, cycles_high;
   asm volatile("RDTSCP\n\t"
                "mov %%edx, %0\n\t"
@@ -23,6 +35,7 @@ static __inline__ ticks cpu_cycles_stop () {
                "CPUID\n\t": "=r" (cycles_high), "=r" (cycles_low):: "%rax",
                              "%rbx", "%rcx", "%rdx");
   return ((ticks)cycles_high << 32) | cycles_low;
+#endif
 }
 
 }
